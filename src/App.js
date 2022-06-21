8c35e64b267d360280c65e802fe328faa0062c6f
ashok3612 <ashok3612@gmail.com>
2022-06-21 12:36:25 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz by Ashok!   
-        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
-      </h1>
+      <h1>Hello StackBlitz by Ashok!</h1>
 
       <p>Start editing to see some magic happen :)</p>
     </div>
